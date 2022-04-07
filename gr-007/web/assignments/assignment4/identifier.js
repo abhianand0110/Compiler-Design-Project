9fa688f3decc994906e8ddf6b892c22a0fbc5052
Rohan Das <bornforcoding777@gmail.com>
2022-04-07 21:19:52 +0530

    Issue #2 Done

---
+++ gr-007/web/assignments/assignment4/identifier.js
@@ -0,0 +1,19 @@
+function checkIdentifier() {
+    var str = document.getElementById('inp').value;
+    // console.log(input);
+
+    if (!((str[0] >= 'a' && str[0] <= 'z') || (str[0] >= 'A' && str[0] <= 'Z') || str[0] == '_')) {
+        document.getElementById('ans').innerHTML = "Not a Valid Identifier";
+        return;
+    }
+
+    for (let i = 1; i < str.length; i++) {
+        if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9') || str[i] == '_')) {
+            document.getElementById('ans').innerHTML = "Not a Valid Identifier";
+            return;
+        }
+    }
+
+    document.getElementById('ans').innerHTML = "Valid Identifier";
+
+}
